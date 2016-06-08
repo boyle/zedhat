e61830d1834cac1648bd3dea2a4b04799f3b4de3
Alistair Boyle <alistair.js.boyle@gmail.com>
2016-06-08 14:37:04 -0400

    libopenblas: test dependency

---
+++ tests/libcblas_dgemm.c
@@ -0,0 +1,39 @@
+/* Copyright 2016, Alistair Boyle, 3-clause BSD License */
+#include <stdio.h>
+#include <math.h>  /* for fabs */
+#include <cblas.h> /* for cblas_dgemm */
+
+int
+main (void)
+{
+    double const a[] = { 0.11, 0.12, 0.13,
+                         0.21, 0.22, 0.23
+                       };
+    double const b[] = { 1011, 1012,
+                         1021, 1022,
+                         1031, 1032
+                       };
+    double c[] = { 0.00, 0.00,
+                   0.00, 0.00
+                 };
+    /* Compute C = a A B + c C */
+    cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                 3, 2, 3, /* m, n, k, */
+                 1.0, a, 3, b, 2, /* a, A, m, B, n, */
+                 0.0, c, 2); /* c, C, k */
+    /* A is m x k, B is k x n, C is m x n */
+    printf ("[ %g, %g\n", c[0], c[1]);
+    printf ("  %g, %g ]\n", c[2], c[3]);
+    double const expect[] = { 367.76, 368.12,
+                              674.06, 674.72
+                            };
+    int i;
+    for(i = 0; i < 4; i++) {
+        if ( fabs(expect[i] - c[i]) > 1e-12 ) {
+            printf("FAIL\n");
+            return 1;
+        }
+    }
+    printf("PASS\n");
+    return 0;
+}
