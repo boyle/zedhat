988d0a8728733ea2a666f0ac280d78a2ebd8b611
Alistair Boyle <alistair.js.boyle@gmail.com>
2016-07-29 13:58:43 -0400

    minor: fix whitespace

--- tests/libmatio.c
+++ tests/libmatio.c
@@ -11,23 +11,23 @@
  *     (1,2)        2
  *     (2,2)        5 */
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
     if( argc != 2 ) {
-       fprintf(stderr, "missing .mat file\n");
-       return 1;
+        fprintf(stderr, "missing .mat file\n");
+        return 1;
     }
     mat_t * matfp = NULL;
     matvar_t * t = NULL;
     if( access( argv[1], F_OK ) == -1 ) {
-       fprintf(stderr, "error: can't find %s\n", argv[1]);
-       return 1;
+        fprintf(stderr, "error: can't find %s\n", argv[1]);
+        return 1;
     }
     printf("hello\n");
     matfp = Mat_Open(argv[1], MAT_ACC_RDONLY);
     if(matfp == NULL) {
-       fprintf(stderr, "error: failed to open %s\n", argv[1]);
-       return 2; /* bad fname */
+        fprintf(stderr, "error: failed to open %s\n", argv[1]);
+        return 2; /* bad fname */
     }
     int more_data = 1;
     int count = 0;
@@ -35,7 +35,6 @@ int main(int argc, char **argv)
         t = Mat_VarReadNextInfo(matfp);
         more_data = (t != NULL);
         if( more_data ) {
-           // Mat_Rewind(matfp);
             matvar_t * tt = Mat_VarRead(matfp, t->name);
             Mat_VarPrint(tt, 1);
             count++;
@@ -43,8 +42,8 @@ int main(int argc, char **argv)
         Mat_VarFree(t);
     }
     if( count == 0 ) {
-       fprintf(stderr, "no variables loaded from %s\n", argv[1]);
-       return 3;
+        fprintf(stderr, "no variables loaded from %s\n", argv[1]);
+        return 3;
     }
     printf("goodbye\n");
     return 0;
