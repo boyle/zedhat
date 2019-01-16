0cd4f57de399722ad89af32a142f73ac14283850
Alistair Boyle <alistair.js.boyle@gmail.com>
2019-01-16 12:33:46 -0700

    tests/model.c: 3D resistor test

--- tests/model.c
+++ tests/model.c
@@ -640,10 +640,7 @@ void test_shape_3d(void ** state)
 {
     int i;
     /* 3D cube:
-     *    all nodes used as in elems
-     * 2D square:
-     *    first four nodes at z=0
-     *    first two elems */
+     *    all nodes used as in elems */
     double nodes[9][3] = {
         {0, 0, 0},
         {1, 0, 0},
@@ -851,7 +848,7 @@ void test_2d_resistor (void ** state)
     m.surfaceelems = &(se[0][0]);
     m.n_se = 4;
     const int gnd = 1;
-    int nnz = calc_Se_n(2) * 2;
+    int nnz = calc_Se_n(2) * m.n_elems;
     int * ii = malloc(sizeof(int) * nnz);
     int * jj = malloc(sizeof(int) * nnz);
     double * ss = malloc(sizeof(double) * nnz);
@@ -871,7 +868,7 @@ void test_2d_resistor (void ** state)
     cholmod_start (&c) ;                /* start CHOLMOD */
     cholmod_triplet Acoo = {
         m.n_nodes, m.n_nodes, nnz, nnz,
-        ii, jj, ss, NULL, +1, 
+        ii, jj, ss, NULL, +1,
         CHOLMOD_INT, CHOLMOD_REAL, CHOLMOD_DOUBLE
     };
     cholmod_sparse * A = cholmod_triplet_to_sparse(&Acoo, nnz, &c);
@@ -882,8 +879,8 @@ void test_2d_resistor (void ** state)
     b = cholmod_ones (A->nrow, 1, A->xtype, &c) ;   /* b = ones(n,1) */
     /* set b->x = bb */
     int i;
-    for ( i = 0; i < 4; i++) {
-        ((double*) b->x)[i] = bb[i];
+    for ( i = 0; i < m.n_nodes; i++) {
+        ((double *) b->x)[i] = bb[i];
     }
     L = cholmod_analyze (A, &c) ;           /* analyze */
     cholmod_factorize (A, L, &c) ;          /* factorize */
@@ -895,10 +892,131 @@ void test_2d_resistor (void ** state)
             cholmod_norm_dense (r, 0, &c)) ;        /* print norm(r) */
     assert_double_equal(norm, 0.0, DBL_EPSILON);
     double * soln = x->x;
-    printf_mat_double("x",4,1, soln);
+    printf_mat_double("x", 4, 1, soln);
     /* check x */
     double expect[4] = { 0, +1, 0, +1 };
-    assert_mat_equal( 4, 1, soln, expect, 10 * DBL_EPSILON);
+    assert_mat_equal( m.n_nodes, 1, soln, expect, 10 * DBL_EPSILON);
+    /* clean up */
+    cholmod_free_factor (&L, &c) ;          /* free matrices */
+    cholmod_free_sparse (&A, &c) ;
+    cholmod_free_dense (&r, &c) ;
+    cholmod_free_dense (&x, &c) ;
+    cholmod_free_dense (&b, &c) ;
+    cholmod_finish (&c) ;               /* finish CHOLMOD */
+    /* clean up */
+    free(ii);
+    free(jj);
+    free(ss);
+}
+
+void test_3d_resistor (void ** state)
+{
+    int i;
+    /* 3D cube:
+     *    all nodes used as in elems */
+    double nodes[9][3] = {
+        {0, 0, 0},
+        {1, 0, 0},
+        {0, 1, 0},
+        {1, 1, 0},
+        {0, 0, 1},
+        {1, 0, 1},
+        {0, 1, 1},
+        {1, 1, 1},
+        {0, 0, 0}, /* guard */
+    };
+    printf_mat_double("nodes", 8, 3, &(nodes[0][0]));
+    /* TODO currently each row of elems must be sorted
+     * or we won't get an upper triangular matrix */
+    /* Our 'elems' starts at 1 which agrees with netgen */
+    int elems[8][4] = { /* from netgen cube.geo */
+        {0, 0, 0, 0}, /* guard */
+        {2, 5, 1, 4},
+        {1, 5, 3, 4},
+        {3, 5, 7, 8},
+        {8, 4, 5, 6},
+        {2, 6, 5, 4},
+        {8, 4, 3, 5},
+        {0, 0, 0, 0}, /* guard */
+    };
+    int * e = &(elems[1][0]);
+    printf_mat_int("elems", 6, 4, e);
+    for( i = 0; i < 6; i++) {
+        qsort(&(e[i * 4]), 4, sizeof(int), &cmp_int_tests);
+    }
+    printf_mat_int("elems (sorted)", 6, 4, e);
+    int bc[12] = {2, 2, 1, 1, 3, 3, 4, 4, 5, 5, 6, 6};
+    int se[12][3] = {
+        {1, 2, 5},
+        {2, 5, 6},
+        {4, 3, 8},
+        {3, 8, 7},
+        {6, 8, 2},
+        {8, 2, 4},
+        {1, 5, 3},
+        {5, 3, 7},
+        {5, 7, 8},
+        {5, 8, 6},
+        {1, 3, 2},
+        {3, 2, 4},
+    };
+    mesh m = { 0 };
+    m.dim = 3;
+    m.elems = &(elems[1][0]);
+    m.nodes = &(nodes[0][0]);
+    m.n_elems = 6;
+    m.n_nodes = 8;
+    m.bc = &(bc[0]);
+    m.surfaceelems = &(se[0][0]);
+    m.n_se = 12;
+    const int gnd = 1;
+    int nnz = calc_Se_n(3) * m.n_elems;
+    int * ii = malloc(sizeof(int) * nnz);
+    int * jj = malloc(sizeof(int) * nnz);
+    double * ss = malloc(sizeof(double) * nnz);
+    int ret = calc_Se(&m, ii, jj, ss);
+    assert_int_equal(ret, 0);
+    int ngnd = calc_gnd(gnd, &nnz, ii, jj, ss);
+    assert_int_not_equal(ngnd, 0);
+    double bb[8] = {0}; /* nodes */
+    ret = calc_stim_neumann(&m, +1, 1, gnd, &(bb[0]));
+    ret += calc_stim_neumann(&m, -1, 2, gnd, &(bb[0]));
+    printf_mat_double("bb", 8, 1, &(bb[0]));
+    /* fwd_solve */
+    cholmod_dense * x, *b, *r ;
+    cholmod_factor * L ;
+    double one [2] = {1, 0}, m1 [2] = {-1, 0} ;     /* basic scalars */
+    cholmod_common c ;
+    cholmod_start (&c) ;                /* start CHOLMOD */
+    cholmod_triplet Acoo = {
+        m.n_nodes, m.n_nodes, nnz, nnz,
+        ii, jj, ss, NULL, +1,
+        CHOLMOD_INT, CHOLMOD_REAL, CHOLMOD_DOUBLE
+    };
+    cholmod_sparse * A = cholmod_triplet_to_sparse(&Acoo, nnz, &c);
+    assert_ptr_not_equal(A, NULL); /* if allocate failed */
+    assert_int_equal(cholmod_check_sparse (A, &c), 1); /* okay matrix? TRUE=1/FALSE=0 */
+    cholmod_print_sparse (A, "A", &c) ;         /* print the matrix */
+    assert_int_not_equal(A->stype, 0); /* A must be symmetric */
+    b = cholmod_ones (A->nrow, 1, A->xtype, &c) ;   /* b = ones(n,1) */
+    /* set b->x = bb */
+    for ( i = 0; i < m.n_nodes; i++) {
+        ((double *) b->x)[i] = bb[i];
+    }
+    L = cholmod_analyze (A, &c) ;           /* analyze */
+    cholmod_factorize (A, L, &c) ;          /* factorize */
+    x = cholmod_solve (CHOLMOD_A, L, b, &c) ;       /* solve Ax=b */
+    r = cholmod_copy_dense (b, &c) ;            /* r = b */
+    cholmod_sdmult (A, 0, m1, one, x, r, &c) ;      /* r = r-Ax */
+    double norm = cholmod_norm_dense (r, 0, &c);
+    printf ("norm(b-Ax) = %8.1e\n",
+            cholmod_norm_dense (r, 0, &c)) ;        /* print norm(r) */
+    assert_double_equal(norm, 0.0, DBL_EPSILON);
+    double * soln = x->x;
+    printf_mat_double("x", m.n_nodes, 1, soln);
+    /* check x */
+    double expect[8] = { 0, 0, +1, +1, 0, 0, +1, +1 };
+    assert_mat_equal( m.n_nodes, 1, soln, expect, 10 * DBL_EPSILON);
     /* clean up */
     cholmod_free_factor (&L, &c) ;          /* free matrices */
     cholmod_free_sparse (&A, &c) ;
@@ -930,6 +1048,7 @@ int main(void)
         cmocka_unit_test(test_bc_2d_neumann),
         cmocka_unit_test(test_bc_3d_neumann),
         cmocka_unit_test(test_2d_resistor),
+        cmocka_unit_test(test_3d_resistor),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
